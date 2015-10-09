4f9f8a1bbec7848a9f89276c6fdab549e649e9a2
badaro42 <r.bizarra@campus.fct.unl.pt>
2015-10-09 14:37:17 +0100

    Atualizadas rotas após signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    root_path(resource)
+  end
+
+  # Overwriting the sign_up redirect path method
+  def after_sign_up_path_for(resource)
+    root_path(resource) # Or :prefix_to_your_route
+  end
 end
