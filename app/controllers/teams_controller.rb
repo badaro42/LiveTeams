437be4d1c378e96dfcc46b2b25cbc5b2d763162c
badaro42 <r.bizarra@campus.fct.unl.pt>
2015-10-18 17:22:07 +0100

    [Equipas] Edição de equipas a funcionar - Já é possivel adicionar e remover elementos às equipas, sendo até possivel deixar as equipas sem nenhum elemento (fará sentido!?)

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,7 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
+  before_action :set_all_users, only: [:new, :edit]
+
   layout "listings"
 
   # GET /teams
@@ -16,11 +18,13 @@ class TeamsController < ApplicationController
   # GET /teams/new
   def new
     @team = Team.new
-    @all_users = User.all
+    # @all_users = User.all
   end
 
   # GET /teams/1/edit
   def edit
+    @users_in_team = @team.users
+    # puts @users_in_team.ids.to_a
   end
 
   # POST /teams
@@ -42,8 +46,8 @@ class TeamsController < ApplicationController
           puts params[:team][:id]
           puts "LOOOOOOOOOOOOOOOL"
 
-          @team_member = TeamMember.new(:user_id => u_id, :team_id => @team.id, :is_leader => false)
-          @team_member.save
+          team_member = TeamMember.new(:user_id => u_id, :team_id => @team.id, :is_leader => false)
+          team_member.save
 
           # dps dos elementos todos inseridos, procura-se aquele que tem um ID igual ao passado no parametro de lider
           # esse registo é entao atualizado e passa a ser o lider da equipa
@@ -64,8 +68,50 @@ class TeamsController < ApplicationController
   # PATCH/PUT /teams/1
   # PATCH/PUT /teams/1.json
   def update
+    users_in_team = @team.users
+
+    # array com os ids de todos os elementos que ja estavam na equipa
+    users_id = users_in_team.ids
+
     respond_to do |format|
       if @team.update(team_params)
+
+        # se nao for passado nenhum user como parametro, é porque nenhum esta seleccionado
+        # e assim são removidos todos os elementos desta equipa
+        if !params[:team][:users].nil?
+
+          # percorre todos os elementos passados como parametro
+          # se o parametro nao estiver em users_id, cria-se uma nova entrada na tabela TeamMembers
+          # se estiver em users_id, o mesmo id é removido do array.
+          # todos os ids que estiverem no array no final do ciclo sao removidos da tabela de membros
+          params[:team][:users].each do |u_id|
+            if !(users_id.include? u_id.to_i)
+              puts u_id
+              team_member = TeamMember.new(:user_id => u_id, :team_id => @team.id, :is_leader => false)
+              team_member.save
+            else
+              users_id.delete(u_id.to_i)
+            end
+
+            # dps dos elementos todos inseridos, procura-se aquele que tem um ID igual ao passado no parametro de lider
+            # esse registo é entao atualizado e passa a ser o lider da equipa
+            #@team_member = TeamMember.all.find_by(:user_id => , :team_id => )
+            #@team_member.is_leader = true
+            #@team_member.save
+          end
+
+          puts users_id.count
+
+          # se ainda estiver algum id em users_id, essas entradas sao removidas da tabela de membros
+          users_id.each do |u_id|
+            # puts TeamMember.where(user_id: u_id).where(team_id: @team.id)
+
+            TeamMember.delete_all(["user_id = ? AND team_id = ?", u_id.to_s, @team.id.to_s])
+          end
+        else
+          TeamMember.delete_all(["team_id = ?", @team.id.to_s])
+        end
+
         format.html { redirect_to @team, notice: 'Team was successfully updated.' }
         format.json { render :show, status: :ok, location: @team }
       else
@@ -89,6 +135,10 @@ class TeamsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_team
     @team = Team.find(params[:id])
+  end
+
+  def set_all_users
+    @all_users = User.all
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
