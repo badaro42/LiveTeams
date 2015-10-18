ed1ceb698fed0449301e33aafc4012f6467800a9
badaro42 <r.bizarra@campus.fct.unl.pt>
2015-10-18 16:08:28 +0100

    [Equipas] É possível adicionar utilizadores às equipas na altura da criação -Falta definir o lider da equipa; -Falta adicionar alguma formatação à página de criação; -Falta adicionar um mapa à pagina da criação para permitir destacá-la para um local.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -16,6 +16,7 @@ class TeamsController < ApplicationController
   # GET /teams/new
   def new
     @team = Team.new
+    @all_users = User.all
   end
 
   # GET /teams/1/edit
@@ -29,6 +30,28 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
+
+        puts "EQUIPA DO CRL1!!!!!!!!!!!!"
+        puts @team.id
+
+        # adicionar os vários membros da equipa, colocar aqui um FOR!
+        # todos os elementos sao adicionados como nao sendo lider
+        params[:team][:users].each do |u_id|
+          puts "LOOOOOOOOOOOOOOOL"
+          puts u_id
+          puts params[:team][:id]
+          puts "LOOOOOOOOOOOOOOOL"
+
+          @team_member = TeamMember.new(:user_id => u_id, :team_id => @team.id, :is_leader => false)
+          @team_member.save
+
+          # dps dos elementos todos inseridos, procura-se aquele que tem um ID igual ao passado no parametro de lider
+          # esse registo é entao atualizado e passa a ser o lider da equipa
+          #@team_member = TeamMember.all.find_by(:user_id => , :team_id => )
+          #@team_member.is_leader = true
+          #@team_member.save
+        end
+
         format.html { redirect_to @team, notice: 'Team was successfully created.' }
         format.json { render :show, status: :created, location: @team }
       else
@@ -63,13 +86,13 @@ class TeamsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_team
+    @team = Team.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def team_params
-      params.require(:team).permit(:name, :latlon)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def team_params
+    params.require(:team).permit(:name, :latlon, users: [:id])
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,10 @@
 class Team < ActiveRecord::Base
   has_many :team_members
   has_many :users, through: :team_members
+
+  accepts_nested_attributes_for :users
+
+  def users=(users)
+    users.reject(&:blank?)
+  end
 end
