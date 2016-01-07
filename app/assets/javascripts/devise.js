aed53fd295405dbf51d153157efbeed38f9de688
badaro42 <r.bizarra@campus.fct.unl.pt>
2016-01-07 15:45:55 +0000

    [Registo] Localização por omissão no caso de geolocalização falhar

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,5 +1,12 @@
 // documento carregado na pagina de login/registo
 
+// coordenadas por omissão, para o caso do browser não suportar geolocalização
+// estas coordenadas apontam para a rotunda central da FCT-UNL
+var default_coords = {
+    latitude: 38.66099459829549,
+    longitude: -9.204398794099689
+};
+
 $(document).ready(function () {
     // apenas permite numeros no campo do numero de telefone
     $(".numeric").numeric();
@@ -17,10 +24,13 @@ $(document).ready(function () {
 
         if (navigator.geolocation) {
             // obtem as coordenadas do utilizador, e coloca-as escondidas no form
-            navigator.geolocation.getCurrentPosition(geoSucess, geoError);
-        } else {
+            navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        }
+        else {
+            populateUserCoords(default_coords);
             noty({
-                text: 'Geolocalização não suportada por este browser!',
+                text: 'Geolocalização não suportada por este browser!\n' +
+                'A usar a localização por omissão: campus da FCT-UNL.',
                 timeout: 3500, type: 'error', layout: 'bottomCenter'
             });
         }
@@ -28,10 +38,8 @@ $(document).ready(function () {
 });
 
 // função executada quando a posição atual é obtida com sucesso
-function geoSucess(position) {
-    var text_coords = "POINT(" + position.coords.longitude + " " + position.coords.latitude + ")";
-    console.log(text_coords);
-    $('#user_latlon').val(text_coords);
+function geoSuccess(position) {
+    populateUserCoords(position.coords);
 
     noty({
         text: 'Localização obtida com sucesso!',
@@ -39,10 +47,19 @@ function geoSucess(position) {
     });
 }
 
+function populateUserCoords(coords) {
+    var text_coords = "POINT(" + coords.longitude + " " + coords.latitude + ")";
+    console.log(text_coords);
+    $('#user_latlon').val(text_coords);
+}
+
 // função executada quando ocorreu algum erro a obter a posição atual
+// uma vez que nao conseguimos obter a localização, usa-se uma por omissão, neste caso o campus da FCT
 function geoError() {
+    populateUserCoords(default_coords);
     noty({
-        text: 'Geolocalização desativada por ordem do utilizador.',
+        text: 'Geolocalização desativada por ordem do utilizador.\n' +
+        'A usar a localização por omissão: campus da FCT-UNL.',
         timeout: 3500, type: 'error', layout: 'bottomCenter'
     });
 }
