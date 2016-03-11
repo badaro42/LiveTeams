e5e55d6ec7630029d2fae325903583bf160add89
badaro42 <r.bizarra@campus.fct.unl.pt>
2016-03-11 16:17:11 +0000

    [App] SSL ativado

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 module TeseGestaoEmergencia
   class Application < Rails::Application
 
+    config.force_ssl = true
+
     config.middleware.use ActionDispatch::Flash
     config.api_only = false
 
